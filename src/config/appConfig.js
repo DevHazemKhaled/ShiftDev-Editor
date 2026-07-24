7c70d9f1587953a12e18f0f614d2d7f486351edc
Developer hazem khaled <hhaazzeemm0123@gmail.com>
2026-07-24 19:02:39 +0300

    Create appConfig.js

---
+++ src/config/appConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    appName: "ShiftDev Studio Pro",
+    version: "1.0.0",
+    defaultPort: 3000,
+    supportedLanguages: ["javascript", "html", "css", "python", "json", "php"]
+};
