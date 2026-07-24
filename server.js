1d158e10a00b3f7daf6c2afcc1494c0dcfab3881
Developer hazem khaled <hhaazzeemm0123@gmail.com>
2026-07-24 18:47:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 
+// تقديم الملفات الثابتة من مجلد public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// مسارات التنقل للصفحات المتعددة
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -15,7 +17,11 @@ app.get('/editor', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'editor.html'));
 });
 
+app.get('/settings', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'settings.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`[ShiftDev Cloud Core] Running on port ${PORT}`);
+    console.log(`[ShiftDev Cloud Studio] Running smoothly on port ${PORT}`);
 });
